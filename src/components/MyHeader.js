9d02b7d82c4f0f7d0c3b8fed36a85638c7f5cf44
irion94 <tomasz.kozub218@gmail.com>
2019-06-04 14:03:57 +0200

    Created header with working onScroll annimation and navigation bar.

--- src/components/MyHeader.js
+++ src/components/MyHeader.js
@@ -1,37 +1,98 @@
 import * as  React from 'react'
 import logo from '../logo_horizontal_alpha.png';
-import {Link} from "react-router-dom";
-import '../App.css'
+import '../App.scss'
+import $ from "jquery";
 
 class MyHeader extends React.Component {
 
+    componentDidMount() {
+        let j$ = $,
+            $nav = j$("#navigation"),
+            $slideLine = j$("#slide-line"),
+            $currentItem = j$(".current-item"),
+            $logoHeight = j$("#logo");
+
+        j$(function () {
+            if ($currentItem[0]) {
+                $slideLine.css({
+                    "width": $currentItem.width(),
+                    "left": $currentItem.position().left
+                });
+            }
+
+            j$($nav).find("li").click(
+                function () {
+                    $slideLine.css({
+                        "width": j$(this).width(),
+                        "left": j$(this).position().left
+                    });
+                },
+            );
+        });
+
+        this.scrollTimer = null;
+        j$(window).scroll(function () {
+            let top = j$(document).scrollTop();
+            clearTimeout(this.scrollTimer);
+            this.scrollTimer = setTimeout(
+                function () {
+                    if (top < 50) {
+                        $logoHeight.animate({height: '150px'});
+                    }
+                    else {
+                        $logoHeight.animate({height: '80px'});
+                    }
+                }, 100);
+        });
+
+
+    }
+
     render() {
         return (
-            <nav
-                style={{height: 50, backgroundColor: 'rgb(58,162,134)'}}
-                className="navbar fixed-top align-top"
+            <header
+                style={{height: 90, backgroundColor: 'rgb(58,162,134)'}}
+                className="navbar fixed-top" id="navigation"
             >
-                <div style={{marginLeft: 20, backgroundColor: 'rgb(58,162,134)'}} className="rounded float-left">
-                    <img height={100} src={logo} alt={"logo"}/>
+                <div style={{marginLeft: 20, backgroundColor: 'rgb(58,162,134)'}} className="rounded row">
+                    <img id="logo" src={logo} alt={"logo"}/>
                 </div>
-                <ul className="navbar">
-                    <Link className="btn-lg btn-green" to={'/'}>
-                        <li className="list-inline-item nav-link disabled">
-                            <button className="bg-transparent border-0 font-weight-light navButtonFontColor">Home</button>
-                        </li>
-                    </Link>
-                    <Link className="btn-lg btn-green" to={'/1'}>
-                        <li className="list-inline-item nav-link disabled">
-                            <button className="bg-transparent border-0 font-weight-light navButtonFontColor">Loose Weight</button>
-                        </li>
-                    </Link>
-                    <Link className="btn-lg btn-green" to={'/2'}>
-                        <li className="list-inline-item nav-link disabled">
-                            <button className="bg-transparent border-0 font-weight-light navButtonFontColor">Add Muscle</button>
-                        </li>
-                    </Link>
-                </ul>
-            </nav>
+                <nav className="nav-fixed-top navbar-expand-sm btn-green">
+                    <div className="collapse navbar-collapse" id="navbarText">
+                        <ul className="navbar-nav mr-auto">
+                            <li className="nav-item active current-item">
+                                <a className="nav-link">Home</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link">Features</a>
+                            </li>
+                            <li className="nav-item btn-green">
+                                <a className="nav-link btn">Pricing</a>
+                            </li>
+                        </ul>
+                        <span id="slide-line"/>
+                    </div>
+                </nav>
+                {/*<ul className="list-group-horizontal">*/}
+                {/*<li className="current-item list-inline-item">*/}
+                {/*<button*/}
+                {/*className="bg-transparent border-0 font-weight-light navButtonFontColor font-weight-bold">Add*/}
+                {/*Muscle*/}
+                {/*</button>*/}
+                {/*</li>*/}
+                {/*<li className="list-inline-item">*/}
+                {/*<button className="bg-transparent border-0 font-weight-light navButtonFontColor">Add Muscle*/}
+                {/*</button>*/}
+                {/*</li>*/}
+                {/*<li className="list-inline-item">*/}
+                {/*<button className="bg-transparent border-0 font-weight-light navButtonFontColor">Add Muscle</button>*/}
+                {/*</li>*/}
+                {/*<li className="list-inline-item">*/}
+                {/*<button className="bg-transparent border-0 font-weight-light navButtonFontColor">Add Muscle*/}
+                {/*</button>*/}
+                {/*</li>*/}
+                {/*</ul>*/}
+            </header>
         )
     }
 }
