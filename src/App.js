7b1a94724cd0e6d2557184deb89b6ace875a2b69
irion94 <tomasz.kozub218@gmail.com>
2019-06-04 23:45:20 +0200

    some corections

--- src/App.js
+++ src/App.js
@@ -1,47 +1,35 @@
 import React from 'react';
 import MyHeader from "./components/MyHeader";
-import {Home} from "./screens/Home";
-import {BrowserRouter as Router} from "react-router-dom";
 import MyFooter from "./components/MyFooter";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faEnvelope} from '@fortawesome/free-solid-svg-icons'
 import ContentItem from "./components/ContentItem";
 import faker from "faker";
 
 const App = () => {
 
-    const myRef = React.createRef() // Create ref object
+    const allRefs = ({
+        firstRef: React.createRef(),
+        secRef: React.createRef(),
+        thRef: React.createRef(),
+    });
 
-    const scrollToRef = (ref) => ref.current && window.scrollTo(0, ref.current.offsetTop)
-
-    React.useEffect(() => { // Add to scroll after component has mounted
-        scrollToRef(myRef)
-    }, []) // passing an empty array useEffect will run only once, similar to componentDidMount
+    const scrollToRef = (ref) => ref.current.scrollIntoView({behavior: 'smooth', block: 'center'})
 
     return (
-            <Router>
-                <MyHeader/>
-                <button style={{marginTop:100}} className="btn" onMouseDown={() => scrollToRef(myRef)}>
-                    lol
-                </button>
-                <div style={{paddingTop: 120, paddingLeft: 70, width: '70%'}} className="container">
-                    <ContentItem photo={'https://unsplash.it/500/300?random'} imageRight/>
-                    <ContentItem photo={'https://unsplash.it/500/300?random'}/>
-                    <ContentItem photo={'https://unsplash.it/500/300?random'} imageRight/>
-                    <ContentItem photo={faker.image.food()}/>
-                    <ContentItem photo={faker.image.food()} imageRight/>
-                    <ContentItem photo={faker.image.food()} />
-                    <ContentItem photo={faker.image.food()} imageRight/>
-                    <ContentItem photo={faker.image.food()} />
-                    <ContentItem forwardRef={myRef} photo={faker.image.food()} imageRight/>
-                    <ContentItem photo={faker.image.food()} />
-                    <div className="fixed-bottom-right">
-                        <FontAwesomeIcon icon={faEnvelope} size="2x" color='white'/>
-                    </div>z
+        <div>
+            <MyHeader refs={allRefs} scrollToRef={scrollToRef}/>
+            <div style={{paddingLeft: 70, paddingTop: 200, paddingBottom: 150, width: '70%'}} className="container-fluid">
+                <ContentItem forwardRef={allRefs.firstRef} image={'https://unsplash.it/500/500?food'} imageRight/>
+                <ContentItem forwardRef={allRefs.secRef} image={'https://unsplash.it/500/500?food'}/>
+                <ContentItem forwardRef={allRefs.thRef} image={'https://unsplash.it/500/450?food'} imageRight/>
+                <div className="fixed-bottom-right">
+                    <FontAwesomeIcon icon={faEnvelope} size="2x" color='white'/>
                 </div>
-                <MyFooter/>
-            </Router>
-        );
-}
+            </div>
+            <MyFooter/>
+        </div>
+    );
+};
 
 export default App;
